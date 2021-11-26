4e454078b5d8d26b9afc30dd31ed80ffdb53b746
Ethan Freeman <ethanfree23@gmail.com>
2021-11-26 12:07:51 -0600

    commit tree 6acc697f0a85888f9b2877aa2efda2aaf1ed3105

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const HQ = 42
+
+function distanceFromHqInBlocks(start) {
+    return start>HQ?start-HQ:HQ-start;
+}
+
+function distanceFromHqInFeet(start) {
+    return distanceFromHqInBlocks(start) * 264
+}
+
+function distanceTravelledInFeet(start, current) {
+    return Math.abs(start-current)*264;
+}
+
+function calculatesFarePrice(start, current){
+    let n = distanceTravelledInFeet(start, current);
+    if (n<=400) {
+        return 0;
+    } else if (n>=400 && n<=2000) {
+        return (n%400)*0.02;
+    } else if (n>2000 && n<2500) {
+        return 25;
+    } else if (n>2500) {
+        return "cannot travel that far"
+    }
+}
+
+console.log(calculatesFarePrice(34, 24))
